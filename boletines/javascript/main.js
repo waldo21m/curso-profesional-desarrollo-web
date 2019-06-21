99c1183724b4081fb64a7a5747ccb17bc2187418
Eduardo Márquez <eduardo20.3263@gmail.com>
2019-06-21 05:40:33 -0400

    Quitar y agregar clases a un elemento con JS

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -18,6 +18,15 @@ let links = document.querySelectorAll(".close");
 links.forEach(function (link) {
     link.addEventListener("click", function (e) {
         e.preventDefault();
+        let content = document.querySelector(".content");
+        
+        content.classList.remove("zoomInUp");
+        content.classList.remove("animated");
+        
+        content.classList.add("zoomOutUp");
+        content.classList.add("animated");
+        //Quitamos y agregamos la clase animated porque sino no ocurre la animación
+
         console.log(":)");
     });
 });
