9d7289f0cd2310730d2f76808a935cc65812dfe0
Eduardo Márquez <eduardo20.3263@gmail.com>
2019-06-21 05:15:07 -0400

    Eventos

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -1,6 +1,22 @@
 //DOM
-let links = document.querySelectorAll("a");
+// let links = document.querySelectorAll("a");
+
+// links.forEach(function (link) {
+//     console.log(link);
+// });
+
+// let celdas = document.querySelectorAll("td");
+
+// celdas.forEach(function (celda) {
+//     celda.addEventListener("click", function () {
+//         console.log("Click");
+//     });
+// });
+
+let links = document.querySelectorAll(".close");
 
 links.forEach(function (link) {
-    console.log(link);
+    link.addEventListener("click", function (e) {
+        console.log(":)");
+    });
 });
